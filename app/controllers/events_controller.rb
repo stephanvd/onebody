25b48f514ff886d3f3bd7d00cd29c3923aa236da
Stephan van Diepen <stephan@vandiepen.info>
2014-09-26 09:40:27 +0200

    Events new action.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,16 @@ class EventsController < ApplicationController
   def index
     @events = @group ? @group.events.future : Event.future
   end
+
+  def new
+  end
+
+  def create
+    @event = @group.events.new(params[:event])
+    if @event.save
+      redirect_to group_event_path(@group, @event)
+    else
+      render action: 'new'
+    end
+  end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -26,4 +26,33 @@ describe EventsController do
       expect(assigns[:events]).to have(2).items
     end
   end
+
+  describe '#new' do
+    before do
+      @group = FactoryGirl.create(:group)
+      @person = FactoryGirl.create(:person)
+      @person.groups << @group
+      get :new, {group_id: @group.id}, {logged_in_id: @person.id}
+    end
+
+    it 'should show the new event form' do
+      expect(response).to be_success
+      post :create, {
+        group_id: @group.id,
+        event: {
+          person_id: @person.id,
+          name: 'My event',
+          description: 'This event is awesome',
+          starts_at: (Date.today.beginning_of_day + 10.hours),
+          ends_at: (Date.today.beginning_of_day + 12.hours),
+        }
+      }, { logged_in_id: @person.id }
+      expect(response).to be_redirect
+      new_event = Event.last
+      expect(new_event.name).to eq('My event')
+      expect(new_event.description).to eq("This event is awesome")
+      expect(new_event.starts_at).to eq(Date.today.beginning_of_day + 10.hours)
+      expect(new_event.ends_at).to eq(Date.today.beginning_of_day + 10.hours)
+    end
+  end
 end
